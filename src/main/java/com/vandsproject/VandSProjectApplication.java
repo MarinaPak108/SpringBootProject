6e02422eb75d9e8e038f32c7d83b75461a7e6346
pakmarina108@gmail.com <pm@mail.ru>
2023-10-04 14:21:09 +0900

    first commit (connection check)

---
+++ src/main/java/com/vandsproject/VandSProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vandsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VandSProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VandSProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vandsproject/VandSProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VandSProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
